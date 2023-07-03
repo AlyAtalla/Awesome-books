ec25e75a8c738eff21b64c840e41e828ee6acdb0
Hsuwaddyhtat22 <121416668+Hsuwaddyhtat22@users.noreply.github.com>
2023-07-03 16:16:11 +0630

    colla js

--- script.js
+++ script.js
@@ -17,7 +17,11 @@ class Store {
     return books;
   }
 
- 
+ static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 
   static removeBook(author) {
     const books = Store.getBooks();
