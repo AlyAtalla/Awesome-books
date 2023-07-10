7a1697d770da9d16be4d96505ba291895354f5e1
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-07-10 12:26:02 +0300

    Fix linter errors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import Book from './modules/book.js';
-import Store from './modules/store.js';
-import ShowBooks from './modules/showBooks.js';
-import { DateTime } from './node_modules/luxon/src/luxon.js';
+import Book from 'modules/book.js';
+import Store from 'modules/store.js';
+import ShowBooks from 'modules/showBooks.js';
+import { DateTime } from 'node_modules/luxon/src/luxon.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   ShowBooks.displayBooks();
