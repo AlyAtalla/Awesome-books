a8c9b1945d8f8616a2428db93f11a109f284ea1d
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-07-10 13:10:40 +0300

    Replace module folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import Book from '/modules/book.js';
-import Store from '/modules/store.js';
-import ShowBooks from '/modules/showBooks.js';
+import Book from '/my-modules/book.js';
+import Store from '/my-modules/store.js';
+import ShowBooks from '/my-modules/showBooks.js';
 import { DateTime } from '/node_modules/luxon/src/luxon.js';
 
 document.addEventListener('DOMContentLoaded', () => {
